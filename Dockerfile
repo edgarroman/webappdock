0edeb4eb6e3db1e1d03fdc770d92317ae3f9e8d2
Edgar Roman <emroman@pbs.org>
2016-08-30 22:37:06 -0400

    Configured NGIX but still need to add main.d and runit

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM emroman/pythondock:uwsgi2
 
+RUN apt-get update
+RUN apt-get install -y nginx
+
 ADD . /webapp_build
 RUN /webapp_build/setup.sh
 
 CMD ["/sbin/my_init"]
 EXPOSE 80 443
-
--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,8 +1,29 @@
-et -e
+#!/bin/bash
+set -e
 source /webapp_build/buildconfig
 
 header "Setting up NGINX"
 
-run apt-get update
-run apt-get install -y nginx
+# Moved to DockerFile for temp quick build
+#run apt-get update
+#run apt-get install -y nginx
 
+# This username should match what is in setup_user.sh
+USER_NAME=webuser
+# Now calculate the variables
+USER_HOME=/home/$USER_NAME
+NGINX_USER_CONF_DIR=$USER_HOME/etc/
+NGINX_CORE_CONF_DIR=/etc/nginx/conf.d/
+
+# Do the system setup. Essentially we are creating the core nginx.conf
+# file as well as creating /etc/nginx/conf.d/ directory that can be used
+# to add numerous separate and distinct .conf files
+run mkdir -p $NGINX_CORE_CONF_DIR
+run cp -r /webapp_build/nginx_core.conf /etc/nginx/nginx.conf
+
+# Do the local user setup
+# By symbolic linking of the user nginx.conf file to the nginx conf directory
+# the user may push updates to nginx without needing root access
+run mkdir -p $NGINX_USER_CONF_DIR
+run cp -r /webapp_build/nginx_user.conf $NGINX_USER_CONF_DIR/nginx.conf
+run ln -s $NGINX_USER_CONF_DIR/nginx.conf $NGINX_CORE_CONF_DIR/nginx.conf
